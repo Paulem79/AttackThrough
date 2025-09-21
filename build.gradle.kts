d781362be3c3c5232e8503e519a4487b3273ecf6
Paulem <66211574+Paulem79@users.noreply.github.com>
2025-09-21 13:00:18 +0200

    PUBLISH fix(modrinth): fixed publish file

--- build.gradle.kts
+++ build.gradle.kts
@@ -38,7 +38,7 @@ modrinth {
     versionName.set("Attack Through ${project.version}")
     versionType.set("release")
     changelog.set(NewGithubChangelog.getChangelog())
-    uploadFile.set(tasks.build)
+    uploadFile.set(tasks.jar)
     gameVersions.addAll(listOf("1.21.8", "1.21.7", "1.21.6", "1.21.5", "1.21.4", "1.21.3", "1.21.2", "1.21.1", "1.21", "1.20.6", "1.20.5", "1.20.4", "1.20.3", "1.20.2", "1.20.1", "1.20", "1.19.4", "1.19.3", "1.19.2", "1.19.1", "1.19", "1.18.2", "1.18.1", "1.18", "1.17.1", "1.17", "1.16.5", "1.16.4", "1.16.3", "1.16.2", "1.16.1", "1.16", "1.15.2"))
     loaders.addAll(listOf("bukkit", "folia", "paper", "purpur", "spigot"))
 }
@@ -60,12 +60,9 @@ java {
     withSourcesJar()
 }
 
-tasks.jar { enabled = false }
-
-
 java {
     toolchain {
-        languageVersion.set(JavaLanguageVersion.of(17)) // Gradle utilise JDK 17
+        languageVersion.set(JavaLanguageVersion.of(8)) // Gradle utilise JDK 17
     }
 }
 
@@ -73,9 +70,6 @@ tasks.withType<JavaCompile> {
     // On cible Java 8 pour la compatibilité du bytecode
     sourceCompatibility = JavaVersion.VERSION_1_8.toString()
     targetCompatibility = JavaVersion.VERSION_1_8.toString()
-
-    // Si tu veux forcer --release 8 (meilleur pour éviter d'utiliser des API >8 par erreur)
-    options.release.set(8)
 
     options.encoding = "UTF-8"
 }
